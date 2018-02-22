508c8951e06a7b98f1d17a15503471b79a29aa3a
gmakersteam <gmakers.team@gmail.com>
2018-02-22 01:37:00 -0500

    modified

--- src/Home.js
+++ src/Home.js
@@ -11,39 +11,39 @@ const Home = () => {
                 <strong>Estas a cargo de un Albergue y/o refugio!</strong> Tomaremos tus datos para contactar
             </Alert>
             <form className="form-group">
-                <Col xs={12}>
+                {/* <Col xs={12}> */}
                     <div className="input-group">
                         <label>Nombre del Albergue</label>
                         {/* <div className="input-group-btn"> </div> */}
                         <input type="text" className="form-control" aria-label="..."/>
                     </div>
-                </Col>
-                <Col xs={12}>
+                {/* </Col> */}
+                {/* <Col xs={12}> */}
                     <div className="input-group">
                         <label>Tel./ Cel.</label>
                         {/* <div className="input-group-btn"> </div> */}
                         <input type="tel" className="form-control" aria-label="..."/>
                     </div>                
-                </Col>
-                <Col xs={12}>
+                {/* </Col> */}
+                {/* <Col xs={12}> */}
                     <div className="input-group">
                         <label>Direccion</label>
                         {/* <div className="input-group-btn"> </div> */}
                         <input type="text" className="form-control" aria-label="..."/>
                     </div>                
-                </Col>
-                <Col xs={12}>
+                {/* </Col>
+                <Col xs={12}> */}
                     <div className="input-group">
                         <label>Distrito/Provincia/Región</label>
                         {/* <div className="input-group-btn"> </div> */}
                         <input type="tel" className="form-control" aria-label="..."/>
                     </div>                
-                </Col>
-                <Col xs={12}>
+                {/* </Col>
+                <Col xs={12}> */}
                     <div className="input-group">
                         <button type='submit' className='btn btn-primary'>Registrar</button>
                     </div> 
-                </Col>
+                {/* </Col> */}
             </form>
         </div>
     )
